436fcf69865b2ac569b22a209da59b76d9184495
VictorBarbosa <victorcavalcantesb@gmail.com>
2025-04-19 16:55:18 -0300

    Atualiza Jogo.py com correções de import e outras mudanças

--- Jogo.py
+++ Jogo.py
@@ -8,3 +8,15 @@ pygame.init()
 largura = 600
 altura = 400
 tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Jogo da cobrinha")
+
+# Cores Do JOgo
+BRANCO = (255,255,255)
+VERDE = (0,255,0)
+PRETO = (0,0,0)
+VERMELHO = (255,0,0)
+
+# Criar um Relogio
+
+
+
